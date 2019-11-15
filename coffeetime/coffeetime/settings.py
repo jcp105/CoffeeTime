532a3d3a1af53653ca891f953fa2332844f57fd8
jpc105 <jpe@cs.odu.edu>
2019-11-15 16:33:01 -0500

    React Initial Setup w/Babel

--- coffeetime/coffeetime/settings.py
+++ coffeetime/coffeetime/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coffee', 
-    'rest_framework'
+    'coffee',
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
